58f834072ab0017f7d2bf9328de5fa6b976de612
mrvalraven <akatiago@gmail.com>
2025-03-06 01:17:38 +0000

    chore: Added extra config files

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier' // Make sure Prettier is last to disable conflicting ESLint rules
+    ],
+  };
